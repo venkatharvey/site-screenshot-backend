c72a9326de5cf6d65665aed4594c1e1661e167b7
venkatharvey <72544994+venkatharvey@users.noreply.github.com>
2021-09-14 15:20:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+const Hapi = require("@hapi/hapi");
+const puppeteer = require("puppeteer");
+
+const init = async () => {
+  
+  
+  const server = Hapi.Server({
+    port: process.env.PORT || "5000",
+    routes: {
+      cors: true,
+    },
+  });
+
+
+  server.route([{
+      method: "POST",
+      path: "/screenshot",
+      handler: async (request, response) => {
+        try{
+        const { url, height, width, format ,quality,fullscreen} = request.payload;
+        const fulscr= fullscreen==="yes"?true:false;
+        const browser = await puppeteer.launch({
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+          waitUntil:'domcontentloaded'
+        });
+        const page = await browser.newPage();
+        await page.goto(`${url}`);
+        await page.setViewport({
+          quality:quality,
+          height: height,
+          width: width,
+          deviceScaleFactor: 1,
+        });
+
+        const image= await page.screenshot({
+          type:`${format}`,
+          fullPage:fulscr,
+        });
+        await browser.close();
+        return image;
+      }
+        catch{
+          console.error();
+        }
+      },
+    },
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "screenshot-api";
+      },
+    },
+  ]);
+  await server.start();
+  console.log(`Server started in: ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+init();
